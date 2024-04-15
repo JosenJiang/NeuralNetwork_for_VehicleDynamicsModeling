632f162afcb6748a2d2832495e0f681e4c255422
Runsong Jiang <jiangrunsong@smartxtruck.com>
2024-04-15 10:12:53 +0800

    modify training process

--- nn_vehicle_model.py
+++ nn_vehicle_model.py
@@ -41,17 +41,16 @@ class VehicleDynamicsModel(nn.Module):
 
 
 class VehicleDynamicsLSTM(nn.Module):
-    def __init__(self, input_size=10, hidden_size=100, output_size=5, num_layers=2):
+    def __init__(self, input_size=10, hidden_size=50, output_size=5):
         super(VehicleDynamicsLSTM, self).__init__()
         self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
         # 初始化隐藏状态和细胞状态
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        h0 = torch.zeros(1, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(1, x.size(0), self.hidden_size).to(x.device)
 
         # 前向传播 LSTM
         out, _ = self.lstm(x, (h0, c0))  # out的形状：(batch_size, seq_length, hidden_size)
@@ -64,7 +63,7 @@ class VehicleDynamicsLSTM(nn.Module):
 
 def data_process():
     # 1. 生成训练数据 tstep = 8 ms
-    path = "/home/plusai/Documents/external_projects/NeuralNetwork_for_VehicleDynamicsModeling/inputs/trainingdata/data_to_train_0.csv"
+    path = "/home/plusai/Documents/workspace/NeuralNetwork_for_VehicleDynamicsModeling/inputs/trainingdata/data_to_train_0.csv"
     df = pd.read_csv(path)
     length = len(df)
     input_column = [
@@ -87,7 +86,9 @@ def data_process():
         "ay_mps2",
     ]
     input_data = df.loc[:length - 2, input_column]  # 输入特征
-    output_data = df.loc[1:, output_column]  # 输出特征
+    output_data = df.loc[:, output_column]  # 输出特征
+    output_data = output_data.diff()
+    output_data = output_data.loc[1:]
     input_data = input_data.to_numpy()
     print("input_data shape: {}".format(input_data.shape))
     output_data = output_data.to_numpy()
@@ -100,7 +101,7 @@ def data_process():
     return input_data, output_data
 
 
-def plot(args, predicted_output, test_output_data):
+def plot(args, predicted_output, test_output_data, name="test_model.png"):
     test_length = predicted_output.shape[0]
     x_list = np.arange(test_length)
     fig = plt.figure(figsize=(20, 20))
@@ -142,7 +143,7 @@ def plot(args, predicted_output, test_output_data):
     ax.legend()
 
     if args.savepic:
-        pic_path = os.path.join(args.output, "model/test_model.png")
+        pic_path = os.path.join(args.output, "model", name)
         plt.savefig(pic_path)
     plt.show()
     plt.close()
@@ -173,11 +174,11 @@ def model_evaluation(args):
     print("Mean Squared Error:", loss.item())
 
     test_length = 3000
-    plot_predicted_output = predicted_output_tensor[:test_length]
+    plot_predicted_output = predicted_output_tensor[:test_length]  # why?
     plot_predicted_output = plot_predicted_output.numpy()
     plot_output_data = test_output_tensor[:test_length]
     plot_output_data = plot_output_data.numpy()
-    plot(args, plot_predicted_output, plot_output_data)
+    plot(args, plot_predicted_output, plot_output_data, "evaluation.png")
 
 
 def model_training(args):
@@ -212,11 +213,12 @@ def model_training(args):
 
     # 2. deep learning model
     model = VehicleDynamicsModel().to(device)
+    # model = VehicleDynamicsLSTM().to(device)
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     criterion = nn.MSELoss()
 
     # 3. training process
-    num_epochs = 150
+    num_epochs = 160
 
     print("start to taining model")
     for epoch in tqdm.trange(num_epochs):
@@ -236,10 +238,15 @@ def model_training(args):
             optimizer.step()
             loss_list.append(loss.item())
 
+        loss_mean = np.mean(loss_list)
+        if loss_mean < 1e-4:
+            print('Epoch [{}/{}], Loss: {:.5f}'.format(epoch+1, num_epochs, loss_mean))
+            print("The loss value is reached")
+            break
         if (epoch + 1) % 5 == 0:
             print(
                 "Epoch [{}/{}], Loss: {:.4f}".format(
-                    epoch + 1, num_epochs, np.mean(loss_list)
+                    epoch + 1, num_epochs, loss_mean
                 )
             )
 
@@ -255,6 +262,7 @@ def model_training(args):
     torch.save(checkpoint, save_path)
 
     # 4. test process
+    model = model.eval()
     test_length = 3000
     test_input_data = x_test[:test_length]
     test_output_data = y_test[:test_length]
@@ -262,7 +270,15 @@ def model_training(args):
         predicted_output = model(test_input_data)
         predicted_output = predicted_output.to("cpu").numpy()
     test_output_data = test_output_data.to("cpu").numpy()
-    plot(args, predicted_output, test_output_data)
+    plot(args, predicted_output, test_output_data, "test_data.png")
+
+    train_input_data = x_train[:test_length]
+    train_output_data = y_train[:test_length]
+    with torch.no_grad():
+        predicted_output = model(train_input_data)
+        predicted_output = predicted_output.to("cpu").numpy()
+    train_output_data = train_output_data.to("cpu").numpy()
+    plot(args, predicted_output, train_output_data, "train_data.png")
 
 
 def main(args):
