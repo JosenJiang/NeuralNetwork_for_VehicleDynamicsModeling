a0f610ca9676d3c3d363241e1bd6df9e752ca9a2
JosenJiang <runsong.jiang@plus.ai>
2024-04-16 10:16:01 +0800

    add lstm model

--- nn_vehicle_model.py
+++ nn_vehicle_model.py
@@ -44,13 +44,14 @@ class VehicleDynamicsLSTM(nn.Module):
     def __init__(self, input_size=10, hidden_size=50, output_size=5):
         super(VehicleDynamicsLSTM, self).__init__()
         self.hidden_size = hidden_size
+        self.num_layers = 1
         self.lstm = nn.LSTM(input_size, hidden_size, batch_first=True)
         self.fc = nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        # 初始化隐藏状态和细胞状态
-        h0 = torch.zeros(1, x.size(0), self.hidden_size).to(x.device)
-        c0 = torch.zeros(1, x.size(0), self.hidden_size).to(x.device)
+        # x.shape = batch, sequence, feature
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
 
         # 前向传播 LSTM
         out, _ = self.lstm(x, (h0, c0))  # out的形状：(batch_size, seq_length, hidden_size)
@@ -61,9 +62,10 @@ class VehicleDynamicsLSTM(nn.Module):
         return out
 
 
-def data_process():
+def data_process(args):
     # 1. 生成训练数据 tstep = 8 ms
-    path = "/home/plusai/Documents/workspace/NeuralNetwork_for_VehicleDynamicsModeling/inputs/trainingdata/data_to_train_0.csv"
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(current_path, "inputs/trainingdata/data_to_train_0.csv")
     df = pd.read_csv(path)
     length = len(df)
     input_column = [
@@ -86,19 +88,32 @@ def data_process():
         "ay_mps2",
     ]
     input_data = df.loc[:length - 2, input_column]  # 输入特征
-    output_data = df.loc[:, output_column]  # 输出特征
-    output_data = output_data.diff()
-    output_data = output_data.loc[1:]
+    output_data = df.loc[1:, output_column]  # 输出特征
+    # output_data = output_data.diff()
+    # output_data = output_data.loc[1:]
     input_data = input_data.to_numpy()
-    print("input_data shape: {}".format(input_data.shape))
     output_data = output_data.to_numpy()
-    print("output_data shape: {}".format(output_data.shape))
 
     scaler = MinMaxScaler()
     input_data = scaler.fit_transform(input_data)
     output_data = scaler.fit_transform(output_data)
+    if args.model == "lstm":
+        input_data, output_data = create_sequences(input_data, output_data, 3)
+    print("input_data shape: {}".format(input_data.shape))
+    print("output_data shape: {}".format(output_data.shape))
 
     return input_data, output_data
+
+
+def create_sequences(input_data, output_data, seq_length):
+    xs, ys = [], []
+    length = input_data.shape[0]
+    for i in range(length - seq_length - 1):
+        x = input_data[i:i + seq_length]
+        y = output_data[i + seq_length - 1]
+        xs.append(x)
+        ys.append(y)
+    return np.array(xs), np.array(ys)
 
 
 def plot(args, predicted_output, test_output_data, name="test_model.png"):
@@ -143,38 +158,48 @@ def plot(args, predicted_output, test_output_data, name="test_model.png"):
     ax.legend()
 
     if args.savepic:
-        pic_path = os.path.join(args.output, "model", name)
+        pic_path = os.path.join(args.output, args.model + name)
         plt.savefig(pic_path)
+    plt.suptitle(name.split(".")[0])
     plt.show()
     plt.close()
 
 
 def model_evaluation(args):
-    input_data, output_data = data_process()
+    input_data, output_data = data_process(args)
     test_input_tensor = torch.from_numpy(input_data.astype(np.float32))
     test_output_tensor = torch.from_numpy(output_data.astype(np.float32))
 
     # 2. deep learning model
     # 加载路径
-    load_path = os.path.join(args.output, "model/model_checkpoint.pth")
+    checkpoint_filename = "{}_model_checkpoint.pth".format(args.model)
+    load_path = os.path.join(args.output, checkpoint_filename)
     # 加载模型
     checkpoint = torch.load(load_path)
     # 重新构建模型
-    model = VehicleDynamicsModel()
+    if args.model == "mlp":
+        model = VehicleDynamicsModel()
+        learning_rate = 0.001
+    elif args.model == "lstm":
+        model = VehicleDynamicsLSTM()
+        learning_rate = 0.01
+    else:
+        raise RuntimeError("Damn! You need to choose a model")
     model.load_state_dict(checkpoint["model_state_dict"])
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     optimizer.load_state_dict(checkpoint["optimizer_state_dict"])
 
     with torch.no_grad():
         model.eval()
-        predicted_output_tensor = model(test_input_tensor)
+        predicted_output_tensor = model(test_input_tensor).squeeze()
 
     criterion = nn.MSELoss()
+    print("predicted_output_tensor.shape = {}".format(predicted_output_tensor.shape))
     loss = criterion(predicted_output_tensor, test_output_tensor)
     print("Mean Squared Error:", loss.item())
 
     test_length = 3000
-    plot_predicted_output = predicted_output_tensor[:test_length]  # why?
+    plot_predicted_output = predicted_output_tensor[:test_length]
     plot_predicted_output = plot_predicted_output.numpy()
     plot_output_data = test_output_tensor[:test_length]
     plot_output_data = plot_output_data.numpy()
@@ -185,7 +210,7 @@ def model_training(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # 设备
     print("device: {}".format(device))
 
-    input_data, output_data = data_process()
+    input_data, output_data = data_process(args)
     x_train, x_test, y_train, y_test = train_test_split(
         input_data, output_data, test_size=0.1, shuffle=False
     )
@@ -202,7 +227,7 @@ def model_training(args):
     train_loader = Data.DataLoader(
         dataset=train_data,  # 使用的数据集
         batch_size=batch_size,
-        shuffle=False,  # 每次迭代前打乱数据
+        shuffle=True,  # 每次迭代前打乱数据
     )
 
     test_loader = Data.DataLoader(
@@ -212,22 +237,29 @@ def model_training(args):
     )
 
     # 2. deep learning model
-    model = VehicleDynamicsModel().to(device)
-    # model = VehicleDynamicsLSTM().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    if args.model == "mlp":
+        model = VehicleDynamicsModel().to(device)
+        learning_rate = 0.001
+    elif args.model == "lstm":
+        model = VehicleDynamicsLSTM().to(device)
+        learning_rate = 0.01
+    else:
+        raise RuntimeError("Damn! You need to choose a model")
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     criterion = nn.MSELoss()
 
     # 3. training process
-    num_epochs = 160
+    max_epochs = 160
 
     print("start to taining model")
-    for epoch in tqdm.trange(num_epochs):
+    loop = tqdm.trange(max_epochs)
+    for epoch in loop:
         loss_list = []
         for inputs, outputs_gt in train_loader:
             inputs = inputs.to(device)
             outputs_gt = outputs_gt.to(device)
             # 前向传播
-            predicted_output = model(inputs)
+            predicted_output = model(inputs).squeeze()
 
             # 计算损失
             loss = criterion(predicted_output, outputs_gt)
@@ -239,16 +271,12 @@ def model_training(args):
             loss_list.append(loss.item())
 
         loss_mean = np.mean(loss_list)
+        loop.set_description(f'Epoch [{epoch}/{max_epochs}]')
+        loop.set_postfix(loss=loss_mean)
         if loss_mean < 1e-4:
-            print('Epoch [{}/{}], Loss: {:.5f}'.format(epoch+1, num_epochs, loss_mean))
+            print('Epoch [{}/{}], Loss: {:.5f}'.format(epoch, max_epochs, loss_mean))
             print("The loss value is reached")
             break
-        if (epoch + 1) % 5 == 0:
-            print(
-                "Epoch [{}/{}], Loss: {:.4f}".format(
-                    epoch + 1, num_epochs, loss_mean
-                )
-            )
 
     # 保存模型参数和其他信息
     checkpoint = {
@@ -257,7 +285,8 @@ def model_training(args):
         # 如果有需要的话，还可以保存其他信息，比如模型的超参数等
     }
     # 选择保存路径
-    save_path = os.path.join(args.output, "model/model_checkpoint.pth")
+    checkpoint_filename = "{}_model_checkpoint.pth".format(args.model)
+    save_path = os.path.join(args.output, checkpoint_filename)
     # 保存模型
     torch.save(checkpoint, save_path)
 
@@ -267,7 +296,7 @@ def model_training(args):
     test_input_data = x_test[:test_length]
     test_output_data = y_test[:test_length]
     with torch.no_grad():
-        predicted_output = model(test_input_data)
+        predicted_output = model(test_input_data).squeeze()
         predicted_output = predicted_output.to("cpu").numpy()
     test_output_data = test_output_data.to("cpu").numpy()
     plot(args, predicted_output, test_output_data, "test_data.png")
@@ -275,7 +304,7 @@ def model_training(args):
     train_input_data = x_train[:test_length]
     train_output_data = y_train[:test_length]
     with torch.no_grad():
-        predicted_output = model(train_input_data)
+        predicted_output = model(train_input_data).squeeze()
         predicted_output = predicted_output.to("cpu").numpy()
     train_output_data = train_output_data.to("cpu").numpy()
     plot(args, predicted_output, train_output_data, "train_data.png")
@@ -307,6 +336,13 @@ if __name__ == "__main__":
         default=False,
         action="store_true",
         help="Whether to generate data plot",
+    )
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="mlp",
+        choices=["mlp", "lstm"],
+        help="choose a NN model to train",
     )
     args = parser.parse_args()
     main(args)
